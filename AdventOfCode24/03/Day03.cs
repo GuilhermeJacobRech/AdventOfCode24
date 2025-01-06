9335db9e20b4ae72a753d3c02c7a05e90bd09954
GuilhermeJacobRech <guilhermejacobrech@gmail.com>
2025-01-06 16:47:32 +0100

    Added Day03 Part 2 solution

--- AdventOfCode24/03/Day03.cs
+++ AdventOfCode24/03/Day03.cs
@@ -14,20 +14,65 @@ namespace AdventOfCode24._03
             // Read lines of input.txt
             var inputLines = File.ReadLines(base.InputPath);
 
-            var ansP1 = SolvePart1(inputLines.ToArray());
-            Console.WriteLine(ansP1);
+            //var ansP1 = SolvePart1(inputLines.ToArray());
+            var ansP2 = SolvePart2(inputLines.ToArray());
+            //Console.WriteLine(ansP1);
+            Console.WriteLine(ansP2);
         }
 
-        private static int SolvePart1(string[] memorySections)
+        private static int SolvePart1(string[] memories)
+        {
+            return CalculateInstructions(memories);
+        }
+
+        private static int SolvePart2(string[] memories)
+        {
+            string[] filteredMemories = new string[memories.Length];
+            int i = 0;
+            bool isEnabled = true;
+
+            foreach (var memory in memories)
+            {
+                string temp = "";
+                string lastSevenChars = "";
+
+                foreach (char c in memory)
+                {
+                    if (lastSevenChars.Contains("don't()")) isEnabled = false;
+                    if (lastSevenChars.Contains("do()")) isEnabled = true;
+
+                    if (isEnabled)
+                    {
+                        // Insert current char at the end the temp string
+                        temp = temp.Insert(temp.Length, c.ToString());
+                    }
+
+                    if (lastSevenChars.Length > 6)
+                    {
+                        // Remove the char at the begining of lastSevenChars
+                        lastSevenChars = lastSevenChars.Remove(0, 1);
+                    }
+                    // Add current chat at the end of lastSevenChars
+                    lastSevenChars = lastSevenChars.Insert(lastSevenChars.Length, c.ToString());
+                    
+                }
+                filteredMemories[i] = temp;
+                i++;
+            }
+
+            return CalculateInstructions(filteredMemories);
+        }
+
+        private static int CalculateInstructions(string[] memories)
         {
             int sum = 0;
             string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
-            foreach (var memorySec in memorySections)
+            foreach (var memory in memories)
             {
                 Regex regex = new(pattern);
-                var instructions = regex.Matches(memorySec);
+                MatchCollection matches = regex.Matches(memory);
 
-                foreach (Match match in instructions)
+                foreach (Match match in matches)
                 {
                     if (match.Success)
                     {
@@ -43,10 +88,6 @@ namespace AdventOfCode24._03
             return sum;
         }
 
-        private static int SolvePart2(string[] reports)
-        {
-            return 0;
-        }
 
     }
 }
--- AdventOfCode24/Program.cs
+++ AdventOfCode24/Program.cs
@@ -8,6 +8,12 @@ namespace AdventOfCode24
         {
             _03.Day03 day03 = new();
             day03.Solve();
+
+            string lastSevenChars = "don't()";
+            bool isEnabled = true;
+            if (lastSevenChars.Contains("don't()")) isEnabled = false;
+            if (lastSevenChars.Contains("do()")) isEnabled = true;
+            Console.WriteLine(isEnabled);
         }
         
     }
